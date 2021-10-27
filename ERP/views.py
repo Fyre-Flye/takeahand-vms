de76bcd890dbc20698cc3d2d6c739560df525628
Gustavo Henrique <gustavohco34@gmail.com>
2021-10-27 01:40:29 -0300

    1:40

--- ERP/views.py
+++ ERP/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(resquest):
-    render(request(resquest, 'index.html'))
+    return render(resquest, 'index.html')
--- takeahand/urls.py
+++ takeahand/urls.py
@@ -19,5 +19,5 @@ from ERP.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', home)
+    path('', home)
 ]
