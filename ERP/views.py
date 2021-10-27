122f70c854785f0c6c8ffdb8b7c2db0d83264215
Gustavo Henrique <gustavohco34@gmail.com>
2021-10-27 18:51:08 -0300

    página de formulário adicionada

--- ERP/views.py
+++ ERP/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(resquest):
     return render(resquest, 'index.html')
+
+def form(resquest):
+    return render(resquest, 'form.html')
--- takeahand/urls.py
+++ takeahand/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ERP.views import home
+from ERP.views import home, form
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
+    path('form/', form),
 ]
