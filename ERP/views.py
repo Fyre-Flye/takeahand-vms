e96fc70a1f9d4d57bdf9ff7ffddf5e3583372841
Artur Teodoro <artur.luiz.teodoro@hotmail.com>
2021-11-10 13:46:31 -0300

    Create and Read

--- ERP/views.py
+++ ERP/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from ERP.forms import VoluntariosForm
-
+from ERP.models import Voluntarios 
 # Create your views here.
 def home(resquest):
-    return render(resquest, 'index.html')
+    data={}
+    data['db'] = Voluntarios.objects.all()
+    return render(resquest, 'index.html', data)
 
 def form(resquest):
     data = {}
     data['form'] = VoluntariosForm()
     return render(resquest, 'form.html', data)
+
+def create(resquest):
+    form = VoluntariosForm(resquest.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(request, pk):
+    data = {}
+    data['db'] = Voluntarios.objects.get(pk=pk)
+    return render(request,'view.html', data)
--- takeahand/urls.py
+++ takeahand/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ERP.views import home, form
+from ERP.views import home, form, create, view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('form/', form),
+    path('', home, name='home'),
+    path('form/', form, name='form'),
+    path('create/', create, name='create'),
+    path('view/<int:pk>/', view, name='view'),
 ]
