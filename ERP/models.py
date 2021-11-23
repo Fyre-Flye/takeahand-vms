f55e9b9e3bfcb7eb1cddf34607aaff4f17a8cd29
Gustavo Henrique <gustavohco34@gmail.com>
2021-11-23 04:46:14 -0300

    sistema de certificados

--- ERP/models.py
+++ ERP/models.py
@@ -10,8 +10,8 @@ class Voluntarios(models.Model):
     funcao = models.CharField(max_length=30)
     bairro = models.CharField(max_length=30) # t 
     telefone = models.CharField(max_length=30) # t
-    horas_semanais = models.CharField(max_length=30)
-    horas_contribuidas = models.CharField(max_length=30)
+    horas_semanais = models.IntegerField()
+    horas_contribuidas = models.IntegerField()
 
     
 
