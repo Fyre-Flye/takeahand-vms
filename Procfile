3a011163c7b13070399e934d3e8d99a37449a05b
Gustavo Henrique <gustavohco34@gmail.com>
2022-01-03 21:56:25 -0300

    Primeiro Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn : gunicorn project.wsgi
--- takeahand/settings.py
+++ takeahand/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,3 +153,5 @@ ACCOUNT_SESSION_REMEMBER = True
 # django-cripsy-forms
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+django_heroku.settings(locals())
